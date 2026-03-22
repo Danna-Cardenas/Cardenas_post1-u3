fd215f863a05d620c374b23c548b26e627f38f65
Danna Sharik Cardenas Hernandez <02220131027@mail.udes.edu.co>
2026-03-21 22:06:29 -0500

    Agregar README_Post1 y todos los cambios recientes

--- src/main/java/decorator/OrdenServicio.java
+++
@@ -1,5 +0,0 @@
-package decorator;
-
-public interface OrdenServicio {
-    String procesarOrden(String ordenId, double monto);
-}
--- src/main/java/decorator/OrdenServicioDecorator.java
+++
@@ -1,9 +0,0 @@
-package decorator;
-
-// Decorator base — delega al componente interno
-public abstract class OrdenServicioDecorator implements OrdenServicio {
-    protected final OrdenServicio wrapped;
-    public OrdenServicioDecorator(OrdenServicio wrapped) {
-        this.wrapped = wrapped;
-    }
-}
